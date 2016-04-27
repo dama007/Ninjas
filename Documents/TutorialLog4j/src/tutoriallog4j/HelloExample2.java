773d629b00079206cc03f477d8cb5267e1597423
mbistuer <m.bistuer.sallan@gmail.com>
2016-04-27 13:33:22 +0200

    Primeros archivos subidos a github.com

---
+++ Documents/TutorialLog4j/src/tutoriallog4j/HelloExample2.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutoriallog4j;
+ import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author usu21
+ */
+public class HelloExample2 {
+   
+
+
+	
+	final static Logger logger = Logger.getLogger(HelloExample2.class);
+	
+	public static void main(String[] args) {
+	
+		HelloExample2 obj = new HelloExample2();
+		
+		try{
+			obj.divide();
+		}catch(ArithmeticException ex){
+			logger.error("Sorry, something wrong!", ex);
+		}
+		
+		
+	}
+	
+	private void divide(){
+		
+		int i = 10 /0;
+
+	}
+	
+}
+    
+
---
+++ Documents/TutorialLog4j/src/tutoriallog4j/TutorialLog4j.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutoriallog4j;
+
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author usu21
+ */
+public class TutorialLog4j {
+
+final static Logger logger = Logger.getLogger(TutorialLog4j.class);
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
